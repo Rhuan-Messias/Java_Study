0fa7e233122ab2ac2360104418642e9107583c03
Messias <110695778+Rhuan-Messias@users.noreply.github.com>
2024-11-29 20:31:41 -0300

    Update Aula4.java

--- classStudy/Aula4.java
+++ classStudy/Aula4.java
@@ -1,5 +1,5 @@
 import java.io.*;
-
+// Escreve no arquivo
 public class ClasseSystemOutFile 
 {
 	
@@ -15,3 +15,24 @@ public class ClasseSystemOutFile
 	}
 	
 }
+
+-------------------------------------------------------------------------------------------------------
+import java.util.Scanner;
+//Le no console
+
+public class ClasseSystemInOut 
+{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite o nome: ");// system só importado dentro do main
+		
+		String nome = scan.nextLine();// le a proxima linha inteira de string
+		System.out.println("Seu nome é: " + nome);
+	}
+	
+}
+
+--------------------------------------------------------------------------------------------------------
+//Le no arquivo
+	
