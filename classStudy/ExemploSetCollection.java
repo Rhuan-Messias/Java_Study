145dc6d60a93ad4890cc9321fe61c68133c91f23
Messias <110695778+Rhuan-Messias@users.noreply.github.com>
2025-01-24 20:21:37 -0300

    Create ExemploSetCollection.java

---
+++ classStudy/ExemploSetCollection.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ExemploSetCollection {
+
+	public static void main(String[] args) {
+		// set é a interface, e possui esses três tipos de collection
+		// hash ordena pela ordem do hashcode
+		// linked ordena na ordem de inserção
+		// Tree ordena pela ordem alfabética ou numérica
+		
+		Set<String> c_hashSet = new HashSet<>();
+		Set<String> c_linkedHashSet = new LinkedHashSet<>();
+		Set<String> c_treeSet = new TreeSet<>();
+		
+		c_hashSet.add("Banana");
+		c_hashSet.add("Maça");
+		c_hashSet.add("Laranja");
+		
+		c_linkedHashSet.add("Banana");
+		c_linkedHashSet.add("Maça");
+		c_linkedHashSet.add("Laranja");
+		
+		c_treeSet.add("Banana");
+		c_treeSet.add("Maça");
+		c_treeSet.add("Laranja");
+		
+		System.out.println("HashSet: " + c_hashSet);
+		System.out.println("LinkedHashSet: " + c_linkedHashSet);
+		System.out.println("TreeSet: " + c_treeSet);
+	}
+
+}
