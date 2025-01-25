512edc480433ba0ae5249eada31e49f159e4c179
Messias <110695778+Rhuan-Messias@users.noreply.github.com>
2025-01-24 21:02:40 -0300

    Update ExemploSetCollection.java

--- classStudy/ExemploSetCollection.java
+++ classStudy/ExemploSetCollection.java
@@ -2,6 +2,7 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.HashSet;
 
 public class ExemploSetCollection {
 
@@ -10,11 +11,13 @@ public class ExemploSetCollection {
 		// hash ordena pela ordem do hashcode
 		// linked ordena na ordem de inserção
 		// Tree ordena pela ordem alfabética ou numérica
-		// A interface Set não permite inserir elementos duplicados (apenas ignora eles)
+		// Set ignora caso insira repetido
 		
 		Set<String> c_hashSet = new HashSet<>();
 		Set<String> c_linkedHashSet = new LinkedHashSet<>();
 		Set<String> c_treeSet = new TreeSet<>();
+		
+		Set<String> c_total = new HashSet<>();
 		
 		c_hashSet.add("Banana");
 		c_hashSet.add("Maça");
@@ -28,9 +31,15 @@ public class ExemploSetCollection {
 		c_treeSet.add("Maça");
 		c_treeSet.add("Laranja");
 		
+		c_total.addAll(c_hashSet);
+		c_total.addAll(c_linkedHashSet);
+		c_total.addAll(c_treeSet);
+		
 		System.out.println("HashSet: " + c_hashSet);
 		System.out.println("LinkedHashSet: " + c_linkedHashSet);
 		System.out.println("TreeSet: " + c_treeSet);
+		
+		System.out.println("HashSet: " + c_total);
 	}
 
 }
